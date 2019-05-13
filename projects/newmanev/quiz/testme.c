79f5f818bc947ed23d435af73a7d04d7f243d1a3
evanrnewman <newmanrevan@gmail.com>
2019-05-12 19:27:08 -0700

    added random tests

---
+++ projects/newmanev/quiz/testme.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    // Only ascii chars in statement (except entire lower alphabet)
+    char set[32] = {'[', '(', '{', ' ', 'a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                   ']', ')', '}'};
+    int r = rand() % 32;
+
+    return set[r];
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+    // Only ascii chars in statement, only fixed length of 5
+    char set[5] = {'r', 'e', 's', 'e', 't'};
+    char *str;
+    char s[5];
+    int i;
+    int r;
+
+    for (i=0; i < 5; i+=1) {
+      r = rand() % 5;
+      s[i] = set[r];
+    }
+    str = s;
+
+    return str;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
